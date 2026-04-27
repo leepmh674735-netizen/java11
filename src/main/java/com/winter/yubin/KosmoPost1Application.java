d6b3400b7fdc999d464a23c65c617323f614f200
Yubin <leepmh674735@gmail.com>
2026-04-27 14:07:44 +0900

    post -1

---
+++ src/main/java/com/winter/yubin/KosmoPost1Application.java
@@ -0,0 +1,13 @@
+package com.winter.yubin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoPost1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoPost1Application.class, args);
+	}
+
+}
