11c03e9159cf5159ed693a7764c44d003a819f69
Yubin <leepmh674735@gmail.com>
2026-05-07 16:34:40 +0900

    22

--- src/main/java/com/winter/yubin/config/InterceotorConfig.java
+++ src/main/java/com/winter/yubin/config/InterceotorConfig.java
@@ -1,51 +1,43 @@
 package com.winter.yubin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.context.annotation.Configuration; // annotation 오타 수정
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry; // Interceptor 오타 수정
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.winter.yubin.interceptors.LoginCheckInterceptor;
-import com.winter.yubin.interceptors.WriterCheckInterceptor;
+import com.winter.yubin.interceptors.WriterCheckInterceptor; // yubin 오타 수정
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Autowired
-    private LoginCheckInterceptor loginCheckInterceptor;
-    
+    private LoginCheckInterceptor loginCheckInterceptor; // 변수명은 소문자로 시작하는 것이 관례입니다.
+
     @Autowired
     private WriterCheckInterceptor writerCheckInterceptor;
-    
+
     @Autowired
     private LocaleChangeInterceptor localeChangeInterceptor;
-    
-    
-    
-    
+
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        
+    public void addInterceptors(InterceptorRegistry registry) { // publiv -> public 수정
+
         // 1. 로그인 체크 인터셉터
-        // 로그인하지 않은 사용자는 아예 접근조차 못하게 막는 첫 번째 관문입니다.
-        registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/member/**")
+        registry.addInterceptor(loginCheckInterceptor) // addInterceptors -> addInterceptor
+                .addPathPatterns("/member/**")          // addPathPatters -> addPathPatterns
                 .addPathPatterns("/qna/**")
-                .excludePathPatterns("/member/login", "/member/join")
-                .excludePathPatterns("/qna/list");
-        
-        registry.addInterceptor(writerCheckInterceptor)
-                .addPathPatters("/notice/update", "/qua/update")
-                
-        registry.addInterceptor(localeChangeInterceptor)
-                 .addInterceptor)
-        
+                .excludePathPatterns("/member/login", "/member/join") // exculdePatterns -> excludePathPatterns
+                .excludePathPatterns("/qna/list");      // qua -> qna 오타 수정
+
         // 2. 작성자 체크 인터셉터
-        // 로그인은 했지만, '남의 글'을 수정/삭제하려는 행위를 막는 두 번째 관문입니다.
-        // 수정(update), 삭제(delete) 경로에만 적용하는 것이 일반적입니다.
         registry.addInterceptor(writerCheckInterceptor)
                 .addPathPatterns("/notice/update", "/notice/delete")
                 .addPathPatterns("/qna/update", "/qna/delete");
+
+        // 3. 언어 설정 변경 인터셉터
+        registry.addInterceptor(localeChangeInterceptor)
+                .addPathPatterns("/**"); // 보통 전체 경로에 적용합니다.
     }
 }
