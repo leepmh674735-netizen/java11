91671cb517643df9a7ed1694efd70fd83b6b20fe
Yubin <leepmh674735@gmail.com>
2026-05-08 18:23:22 +0900

    V2

--- src/main/java/com/winter/yubin/schedule/TestSchedule.java
+++ src/main/java/com/winter/yubin/schedule/TestSchedule.java
@@ -4,19 +4,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@EnableScheduling
-@Component
+@EnableScheduling // 1. 스케줄링 기능 활성화
+@Component        // 2. 스프링 빈으로 등록
 public class TestSchedule {
 	
-	@Scheduled(fixedRateString = "2000", initialDelay = 1000 )
-	public void useFixRate()throws Exception{
-		System.out.println(x:"고정 간격으로 반복 할 코드");
-		
-	} 
+    // 3. 고정 주기 실행 (작업 시작 시점 기준)
+    @Scheduled(fixedRateString = "2000", initialDelay = 1000)
+    public void useFixRate() throws Exception {
+        System.out.println("고정 간격(Rate)으로 반복 할 코드");
+    } 
 	
-	@Scheduled(fixedDelay = 2000, intialDealyString = "2000")
-	public void userFixDelay()throws Exception{
-		System.out.println(x: "고정 간격으로 반복 Delay");
-		
-	}
+    // 4. 고정 지연 실행 (작업 종료 시점 기준)
+    @Scheduled(fixedDelay = 2000, initialDelayString = "2000")
+    public void userFixDelay() throws Exception {
+        System.out.println("고정 지연(Delay)으로 반복 코드");
+    }
 }
